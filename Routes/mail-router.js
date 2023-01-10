63b79f44fd74931fa332bd195c28abc1dd20911a
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-10 17:14:11 +0530

    api update

--- Routes/mail-router.js
+++ Routes/mail-router.js
@@ -6,10 +6,10 @@ const meeetingController = require('../Controllers/controller')
 
 const storage = require('../Helpers/storage')
 
-// router.post('/send-career-mail', storage, mailController.careerMail);
+router.post('/send-career-mail', storage, mailController.careerMail);
 
-// router.post('/send-client-mail', mailController.clientMail);
+router.post('/send-client-mail', mailController.clientMail);
 
-router.post('/send-Meeting-mail' , meeetingController.sendemail)
+
 
 module.exports = router;
