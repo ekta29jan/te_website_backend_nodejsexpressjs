88409b4af15c432a1e0bf3fb7089cfe438baf8c3
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-18 18:22:13 +0530

    bug fix

--- app.js
+++ app.js
@@ -7,21 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(cors({
     origin: '*'
 }));
-var allowedOrigins = [
-                      'https://zany-plum-millipede-gown.cyclic.app'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+
 app.use(
     express.urlencoded({
         extended: false,
