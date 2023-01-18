c4cd9c9d4c03df4fbc96f426943e3806be1a4873
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-18 13:21:21 +0530

    meeting code update

--- Routes/mail-router.js
+++ Routes/mail-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const mailController = require('../Controllers/mail-controller');
-
+const meetingController = require('../Controllers/meetingController')
 
 const storage = require('../Helpers/storage')
 
@@ -10,6 +10,6 @@ router.post('/send-career-mail', storage, mailController.careerMail);
 
 router.post('/send-client-mail', mailController.clientMail);
 
-
+router.post('/send-meeting-mail',meetingController.meetingMail)
 
 module.exports = router;
