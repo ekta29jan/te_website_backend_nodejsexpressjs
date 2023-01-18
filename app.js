e62dc2018cc2778511624572505e36bb9655737c
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-18 16:39:30 +0530

    backend code update

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ const port = process.env.PORT || 3000;
 app.use(cors({
     origin: '*'
 }));
-
+var allowedOrigins = ['http://localhost:3000',
+                      'https://zany-plum-millipede-gown.cyclic.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(
     express.urlencoded({
         extended: false,
@@ -20,10 +34,7 @@ app.use(
     })
 );
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+
 const mail = require('./Routes/mail-router');
 
 //cors middleware
