d31c2582bcc164f1b2d696510659b03923677b1a
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-19 10:58:13 +0530

    update

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
-let corsOptions = {
-    origin : ['https://zany-plum-millipede-gown.cyclic.app'],
- }
- 
- app.use(cors(corsOptions))
 
+var allowedOrigins = [
+                      'https://zany-plum-millipede-gown.cyclic.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(
     express.urlencoded({
         extended: false,
